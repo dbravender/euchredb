4f44f80c0a3bcc21d2da0ea9bcf9d0c8b13a285a
Dan Bravender <dan.bravender@gmail.com>
2017-06-05 08:45:18 +0900

    Search for particular hands

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from itertools import repeat
 import sqlite3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
@@ -10,13 +11,18 @@ c = conn.cursor()
 
 
 @app.route('/')
-def hello_world():
+def best_first_card():
+    cards = request.args.getlist('card')
+    cards = ['%%%s%%' % card for card in cards[:5]]
+    cards += list(repeat('%%%%', 5 - len(cards)))
     result = c.execute("select card as card, rank as rank, count(*) as count, "
                        "avg(ucb) as average_ucb, sum(wins) * 1.0 / sum(visits) "
                        "as win_percentage, sum(wins) as wins, sum(visits) as "
                        "visits from hand_cards where hands_by_card_id in "
                        "(select rowid from hands_with_cards where cards like "
-                       "'%jd%' and cards like '%jh%' and cards like '%0s%' "
-                       "and cards like '%9s%' and cards like '%js%') "
-                       "group by card, rank order by rank, count(*) desc;")
+                       "? and cards like ? and cards like ? "
+                       "and cards like ? and cards like ?) "
+                       "group by card, rank order by rank, count(*) desc "
+                       "limit 5",
+                       cards)
     return jsonify(map(dict, result.fetchall()))
