a7ddc85b68872067c0f68f29a145e10f9e9ef6cf
Dan Bravender <dan.bravender@gmail.com>
2017-06-05 15:43:26 +0900

    Rough front end

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from itertools import repeat
 import sqlite3
 
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -12,17 +12,19 @@ c = conn.cursor()
 
 @app.route('/')
 def best_first_card():
-    cards = request.args.getlist('card')
-    cards = ['%%%s%%' % card for card in cards[:5]]
-    cards += list(repeat('%%%%', 5 - len(cards)))
-    result = c.execute("select card as card, rank as rank, count(*) as count, "
-                       "avg(ucb) as average_ucb, sum(wins) * 1.0 / sum(visits) "
-                       "as win_percentage, sum(wins) as wins, sum(visits) as "
-                       "visits from hand_cards where hands_by_card_id in "
-                       "(select rowid from hands_with_cards where cards like "
-                       "? and cards like ? and cards like ? "
-                       "and cards like ? and cards like ?) "
-                       "group by card, rank order by rank, count(*) desc "
-                       "limit 5",
-                       cards)
-    return jsonify(map(dict, result.fetchall()))
+    cards = request.args.getlist('card')[:5]
+    cards += list(repeat('', 5 - len(cards)))
+    query_cards = ['%%%s%%' % card for card in cards]
+    results = c.execute("select card as card, rank as rank, count(*) as count, "
+                        "avg(ucb) as average_ucb, sum(wins) * 1.0 / sum(visits) "
+                        "as win_percentage, sum(wins) as wins, sum(visits) as "
+                        "visits from hand_cards where hands_by_card_id in "
+                        "(select rowid from hands_with_cards where cards like "
+                        "? and cards like ? and cards like ? "
+                        "and cards like ? and cards like ?) "
+                        "group by card, rank order by rank, count(*) desc "
+                        "limit 5",
+                        query_cards)
+    return render_template('index.html',
+                           cards=cards,
+                           results=map(dict, results))
